bf75f9180e1e035f86662175294567336e1039fc
Emon <146737722+Nh-emon@users.noreply.github.com>
2024-08-09 19:38:31 +0600

    Add files via upload

---
+++ test.js
@@ -0,0 +1,37 @@
+<div id="default-content-on-desktop" class="col-12">
+  <div class="px-1 px-md-2 pb-4 mb-3" data-eltype="section"><div class="col-12">
+                <div class="scroll py-3 scroll-both scroll-min scroll-on-hover" data-eltype="table-container">
+                <table class="table table-borderless mx-auto" data-enableeditcontent="true" data-eltype="table">
+            <thead class="table-active table-border">
+             <tr>
+             <th data-enableeditcontent="true" scope="col" class="">key</th><th data-enableeditcontent="true" scope="col" class="">Action</th>
+             </tr>
+            </thead>
+            
+            <tbody>
+            <tr>
+                        <th data-enableeditcontent="true" scope="row" class=""><span style="" class="hl hl-default">ctrl</span> + <span style="" class="hl hl-default">1</span></th><td data-enableeditcontent="true" class="">section</td>
+                        </tr><tr>
+                        <th data-enableeditcontent="true" scope="row" class=""><span style="" class="hl hl-default">ctrl</span> + <span style="" class="hl hl-default">2</span></th><td data-enableeditcontent="true" class="">heading</td>
+                        </tr><tr>
+                        <th data-enableeditcontent="true" scope="row" class=""><span style="" class="hl hl-default">ctrl</span> + <span style="" class="hl hl-default">3</span></th><td data-enableeditcontent="true" class="">paragraph</td>
+                        </tr><tr>
+                        <th data-enableeditcontent="true" scope="row" class=""><span style="" class="hl hl-default">ctrl</span> + <span style="" class="hl hl-default">4</span></th><td data-enableeditcontent="true" class="">list</td>
+                        </tr><tr>
+                        <th data-enableeditcontent="true" scope="row" class=""><span style="" class="hl hl-default">ctrl</span> + <span style="" class="hl hl-default">5</span></th><td data-enableeditcontent="true" class="">table</td>
+                        </tr><tr>
+                        <th data-enableeditcontent="true" scope="row" class=""><span style="" class="hl hl-default">ctrl</span> + <span style="" class="hl hl-default">6</span></th><td data-enableeditcontent="true" class="">media</td>
+                        </tr><tr>
+                        <th data-enableeditcontent="true" scope="row" class=""><span style="" class="hl hl-default">ctrl</span> + <span style="" class="hl hl-default">7</span></th><td data-enableeditcontent="true" class="">code</td>
+                        </tr><tr>
+                        <th data-enableeditcontent="true" scope="row" class=""><span style="" class="hl hl-default">ctrl </span>+ <span style="" class="hl hl-default">8</span></th><td data-enableeditcontent="true" class="">accordion</td>
+                        </tr><tr>
+                        <th data-enableeditcontent="true" scope="row" class=""><span style="" class="hl hl-default">ctrl </span>+ <span style="" class="hl hl-default">9</span></th><td data-enableeditcontent="true" class="">tab</td>
+                        </tr><tr>
+                        <th data-enableeditcontent="true" scope="row" class=""><span style="" class="hl hl-default">ctrl</span> +<span style="" class="hl hl-default"> 0</span></th><td data-enableeditcontent="true" class="">alert</td>
+                        </tr>
+            <tr><td data-enableeditcontent="true" class=""><span style="" class="hl hl-default">tab</span></td><td data-enableeditcontent="true" class="">edit content</td></tr><tr></tr><tr><td data-enableeditcontent="true" class=""><span style="" <span="" class="hl hl-default">ctrl</span>&nbsp;+ <span style="" class="hl hl-default">c</span></td><td data-enableeditcontent="true" class="">copy content</td></tr><tr></tr><tr><td data-enableeditcontent="true" class=""><span style="" class="hl hl-default">ctrl</span> + <span style="" class="hl hl-default">v</span></td><td data-enableeditcontent="true" class="">paste element</td></tr><tr></tr><tr><td data-enableeditcontent="true" class=""><span style="" class="hl hl-default">delete</span></td><td data-enableeditcontent="true" class="">delete element</td></tr><tr></tr></tbody>
+            </table>        
+                <span></span><span></span></div>
+                </div></div>
+</div>
